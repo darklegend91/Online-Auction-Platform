ccca69d86f282dfb0e0d86266cf9dc8869021bb1
bhavishya2601 <bgarg2050bhav@gmail.com>
2024-02-03 12:23:13 +0530

    changes

--- styles/script.js
+++ styles/script.js
@@ -2,7 +2,7 @@ function next_page(){
     let a = prompt("Place your Bid");
     let b = confirm("The Amount entered is " + a + "\nDo you wanna Continue?");
     if (b){
-        window.location.href="pages/confirmation.html";
+        window.location.href="../pages/confirmation.html";
     }
 }
 
@@ -12,11 +12,11 @@ function check_Password() {
     if ((a=="") || (b=="")){
         alert("Please Fill out all the details");
     } else if (a == b) {
-        window.location.href = "pages/3_main.html";
+        window.location.href = "../pages/3_main.html";
     } else {
         alert("Password and confirm password does not match.");
     }
 }
 function form_page(){
-    window.location.href="pages/2_form.html";
+    window.location.href="../pages/2_form.html";
 }
